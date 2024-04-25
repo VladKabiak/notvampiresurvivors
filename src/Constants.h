3778ed48ea46f4832a59a16bf39d971daeb7bbc0
Uladzislau <kobiakulad@gmail.com>
2024-04-25 20:31:17 +0200

    fixed health bag

--- src/Constants.h
+++ src/Constants.h
@@ -3,8 +3,8 @@
 
 #include <string>
 
-const std::string TEXTURES_PATH = "../assets/textures/textures.png";
-const std::string FONT_PATH = "../assets/fonts/Arial.ttf";
-const std::string WALK_PATH = "../assets/textures/Walk.png";
+const std::string TEXTURES_PATH = "../../assets/textures/textures.png";
+const std::string FONT_PATH = "../../assets/fonts/Arial.ttf";
+const std::string WALK_PATH = "../../assets/textures/Walk.png";
 
 #endif // CONSTANTS_H
